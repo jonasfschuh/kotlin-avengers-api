7494d9b2f2b3e7f5491c825e752d421ed929ca22
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-03 13:11:07 -0300

    Avenger requests and response. domains.

--- src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/application/web/resource/AvengerResource.kt
@@ -2,7 +2,10 @@ package io.github.jonasfschuh.kotlin_avengers_api.application.web.resource
 
 import io.github.jonasfschuh.kotlin_avengers_api.application.web.resource.request.AvengerRequest
 import io.github.jonasfschuh.kotlin_avengers_api.application.web.resource.response.AvengerResponse
+import io.github.jonasfschuh.kotlin_avengers_api.domain.avenger.Avenger
+import io.github.jonasfschuh.kotlin_avengers_api.domain.avenger.AvengerRepository
 import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,14 +16,29 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = ["v1/api/avenger"])
-class AvengerResource {
+class AvengerResource(
+    @Autowired private val repository: AvengerRepository
+) {
 
     @GetMapping
-    fun getAvengers() = ResponseEntity.ok().body(emptyList<AvengerResponse>())
+    fun getAvengers() = repository.getAvengers()
+        .map { AvengerResponse.from(it) }
+        .let {
+            ResponseEntity.ok().body(it)
+        }
 
     @GetMapping(value =  "{id}")
-    fun getAvengersDetails(@PathVariable( value = "id") id: Long) = ResponseEntity.ok().build<AvengerResponse>()
+    fun getAvengersDetails(@PathVariable( value = "id") id: Long) =
+        repository.getDetail(id).let {
+            ResponseEntity.ok().body(AvengerResponse.from(it))
+        }
 
     @PostMapping
     fun createAvenger(@Valid @RequestBody request: AvengerRequest) =
+        request.toAvenger().run {
+            repository.create(this)
+        }.let {
+            ResponseEntity<Avenger>
+        }
+
 }
