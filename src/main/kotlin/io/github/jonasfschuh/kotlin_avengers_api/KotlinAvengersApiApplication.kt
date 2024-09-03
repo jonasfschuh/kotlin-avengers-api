61881337c86164510ad26ef616e4bef654c7f663
kenzor1979 <jonasschuh@hotmail.com>
2024-09-02 23:12:49 -0300

    Initial commit

---
+++ src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/KotlinAvengersApiApplication.kt
@@ -0,0 +1,11 @@
+package io.github.jonasfschuh.kotlin_avengers_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAvengersApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAvengersApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/jonasfschuh/kotlin_avengers_api/KotlinAvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.jonasfschuh.kotlin_avengers_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
