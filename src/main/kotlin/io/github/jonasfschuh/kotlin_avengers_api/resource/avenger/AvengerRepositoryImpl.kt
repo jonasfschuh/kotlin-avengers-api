9b695ade2ad231825db86c11e7b4e700740c563e
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-04 12:20:45 -0300

    created AvengerEntityRepositoryImpl

--- src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerRepositoryImpl.kt
+++ src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerRepositoryImpl.kt
@@ -2,9 +2,27 @@ package io.github.jonasfschuh.kotlin_avengers_api.resource.avenger
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import io.github.jonasfschuh.kotlin_avengers_api.domain.avenger.Avenger;
+import io.github.jonasfschuh.kotlin_avengers_api.domain.avenger.AvengerRepository
+import org.springframework.data.repository.findByIdOrNull
 
 @Component
-class AvengerRepositoryImpl {
-    @Autowired private val repository: AvengerEntityRepository
+class AvengerRepositoryImpl (
+    @Autowired
+    private val repository: AvengerEntityRepository
+) : AvengerRepository {
+    override fun getDetail(id: Long): Avenger? =
+        repository.findByIdOrNull(id)?.toAvenger()
 
+    override fun getAvengers(): List<Avenger> =
+        repository.findAll().map { it.toAvenger() }
+
+    override fun create(avenger: Avenger): Avenger =
+        repository.save(AvengerEntity.from(avenger)).toAvenger()
+
+    override fun delete(id: Long) =
+        repository.deleteById(id)
+
+    override fun update(avenger: Avenger): Avenger =
+        repository.save(AvengerEntity.from(avenger)).toAvenger()
 }
