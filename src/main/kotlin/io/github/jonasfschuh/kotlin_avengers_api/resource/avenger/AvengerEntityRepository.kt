9b39538d81a3b9b88a803252fe2b001349ede5f9
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-04 12:20:06 -0300

    created AvengerEntityRepository

--- src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerEntityRepository.kt
+++ src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerEntityRepository.kt
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AvengerEntityRepository : JpaRepository<AvengerEntity, Long>  {
-}
+interface AvengerEntityRepository : JpaRepository<AvengerEntity, Long>
