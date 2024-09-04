f1cde6dd57399d3460b577bead17e8e2441d869b
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-04 12:19:25 -0300

    created AvengerEntity

--- src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerEntity.kt
+++ src/main/kotlin/io/github/jonasfschuh/kotlin_avengers_api/resource/avenger/AvengerEntity.kt
@@ -11,7 +11,7 @@ import jakarta.persistence.Id
 data class AvengerEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Long?,
     @Column(nullable = false)
     val nick: String,
     @Column(nullable = false)
@@ -20,4 +20,14 @@ data class AvengerEntity(
     val history: String?
 ) {
     fun toAvenger() = Avenger(id, nick, person, description, history)
+
+    companion object {
+        fun from(avenger: Avenger) = AvengerEntity (
+            id = avenger.id,
+            nick = avenger.nick,
+            person = avenger.person,
+            description = avenger.description,
+            history = avenger.history
+        )
+    }
 }
